c31e9e012e23c6d4acc235d3d70874ee84729350
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-22 14:19:00 -0300

    :tada: initial commit

--- src/jobs.py
+++ src/jobs.py
@@ -16,3 +16,8 @@ def read(path):
         List of rows as dicts
     """
     return []
+
+# A função deve receber um path (uma string com o caminho para um arquivo).
+# A função deve abrir o arquivo e ler seus conteúdos.
+# A função deve tratar o arquivo como CSV.
+# A função deve retornar uma lista de dicionários, onde as chaves são os cabeçalhos de cada coluna e os valores correspondem a cada linha.
