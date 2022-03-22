98486c98fc8dd6e35a692ad1e0cc39e01d14147e
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-22 14:50:39 -0300

    feat: :spartkles: finish first requirement

--- src/jobs.py
+++ src/jobs.py
@@ -1,23 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
-
-# A função deve receber um path (uma string com o caminho para um arquivo).
-# A função deve abrir o arquivo e ler seus conteúdos.
-# A função deve tratar o arquivo como CSV.
-# A função deve retornar uma lista de dicionários, onde as chaves são os cabeçalhos de cada coluna e os valores correspondem a cada linha.
+    with open(path, "r") as file:
+        jobsStructured = csv.DictReader(file)
+        array = []
+        for item in jobsStructured:
+            array.append(item)
+        return array
