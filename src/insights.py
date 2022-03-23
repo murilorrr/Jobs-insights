8292a3e1cd94677dc4a3e6a44199ad75a6d30f00
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-23 15:51:14 -0300

    fix: fixxed flak8 linter

--- src/insights.py
+++ src/insights.py
@@ -1,4 +1,3 @@
-from unicodedata import digit
 from src.jobs import read
 
 
@@ -69,29 +68,6 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-
     try:
         if job["min_salary"] <= int(salary) <= job["max_salary"]:
             return True
